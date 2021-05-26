e466659d165dc57eceacef0914849e6bdb7a732e
Zharkynbek Idirisov <zh.idirisov@gmail.com>
2021-05-26 20:20:07 +0200

    react training

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Painting from "./components/Painting"
+
+const App = () => {
+    return (
+      <div>
+            <h1>First Component</h1>
+            <Painting/>
+      </div>
+    );
+}
+
+export default App;
---
+++ src/components/Painting.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Painting = () => {
+    return <div>Component images</div>
+}
+
+export default Painting;
