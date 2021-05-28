0ce357d4d5cf7e07e73ddda56f07cba06328b3de
Zharkynbek Idirisov <zh.idirisov@gmail.com>
2021-05-28 21:04:03 +0200

    react training

--- src/App.js
+++ src/App.js
@@ -1,58 +1,29 @@
 import React from "react"
-import Painting from "./components/Painting"
+import PaintingList from "./components/PaintingList";
 import paintings from "./paintings.json"
-
-console.log(paintings);
-
-
-const painting = {
-  id: "id-1",
-  url: "https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg",
-  title: "Feathers. Art abstract vivalaviva",
-  price: 500,
-  author: {
-    tag: "ractapopulous",
-    url: "https://pixabay.com/users/ractapopulous-24766/",
-  },
-  quantity: 5,
-};
-
-const painting2 = {
-  id: "id-2",
-  url: "https://cdn.pixabay.com/photo/2017/08/02/22/38/bird-2573779_1280.jpg",
-  title: "Bird. Animal art abstract",
-  price: 6444,
-  author: {
-    tag: "ractapopulous",
-    url: "https://pixabay.com/users/ractapopulous-24766/",
-  },
-  quantity: 15,
-};
-
+import Logo from "./components/Logo";
+import Panel from "./components/Panel"
 
 const App = () => {
     return (
       <div>
-        <h1>First Component</h1>
-        <Painting
-          url={paintings[0].url}
-          title={paintings[0].title}
-          a={5}
-          b={10}
-          price={paintings[0].price}
-          profileUrl={paintings[0].author.url}
-          tag={paintings[0].author.tag}
-          quantity={paintings[0].quantity}
-        />
-        <Painting
-          url="https://cdn.pixabay.com/photo/2017/08/02/22/38/bird-2573779_1280.jpg"
-          alt="Feathers. Art abstract vivalaviva"
-          price={400}
-          title="Bird. Animal art abstract"
-          profileUrl="https://pixabay.com/users/ractapopulous-24766/"
-          tag="ractapopulos"
-          quantity={paintings[2].quantity}
-        />
+        <Panel title="last news">
+          <p>
+            lorem ipsums sdklöfjsdlk sdofsadöoifh disofjsiodfjs sdiofjsdiofj
+            sdiofjsiodfj iofjas dfoij sdpiofjsdi fsiodf
+            siofjsdiofjsiodfjsiofjsdifjsödfjsadiöfjasiofjwfjks+oü#kdofksdoäfjsdfg
+          </p>{" "}
+          <a href="">lesen...</a>
+        </Panel>
+        <Panel>
+          <p>
+            lorem ipsums sdklöfjsdlk sdofsadöoifh disofjsiodfjs sdiofjsdiofj
+            sdiofjsiodfj iofjas dfoij sdpiofjsdi fsiodf
+            siofjsdiofjsiodfjsiofjsdifjsödfjsadiöfjasiofjwfjks+oü#kdofksdoäfjsdfg
+          </p>{" "}
+        </Panel>
+        <Logo text={<h1>First Component</h1>} />
+        <PaintingList paintings={paintings} />
       </div>
     );
 }
---
+++ src/components/Logo.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const Logo = ({ text }) => <div>{text}</div>
+
+export default Logo;
