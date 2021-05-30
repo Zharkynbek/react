37e14f5fa61877218e47f00cd0a587d1ffd5987a
Zharkynbek Idirisov <zh.idirisov@gmail.com>
2021-05-30 13:50:17 +0200

    change settings

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import paintings from "./paintings.json"
 import Logo from "./components/Logo";
 import Panel from "./components/Panel"
 
+const colorPickerOptions = [
+  { label: "red", color: "#F44336" },
+  { label: "green", color: "#4CAF50" },
+  { label: "blue", color: "#2196F3" },
+  { label: "grey", color: "#607D8B" },
+  { label: "pink", color: "#E91E63" },
+  { label: "indigo", color: "#3F51B5" },
+];
+console.log(colorPickerOptions);
+
 const App = () => {
     return (
       <div>
